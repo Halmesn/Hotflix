cdd00522dc33958cea0be619981da612fd8bd945
Adrian Li <xiaxi.li.syd@gmail.com>
2021-05-05 01:19:43 +1000

    create signUpForm component

---
+++ components/home/signup-form/SignUpForm.js
@@ -0,0 +1,11 @@
+import * as styled from './SignUpStyles';
+
+export default function SignUpForm() {
+  return (
+    <styled.SignUpForm>
+      <styled.Title>
+        Ready to watch Netflix? Enter your email to start Today!
+      </styled.Title>
+    </styled.SignUpForm>
+  );
+}
