43d5b5d6da1e0a2659ac5a405791aba7d1d1ba89
Adrian Li <xiaxi.li.syd@gmail.com>
2021-05-08 01:41:10 +1000

    add authorizationUrl to Google provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -41,6 +41,8 @@ export default NextAuth({
     Providers.Google({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+      authorizationUrl:
+        'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
     }),
   ],
 });
